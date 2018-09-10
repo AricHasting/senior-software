9f243e17a370b7587a343c292e7c8a985c2bb8bc
VelascoAMath <alfredo0234@gmail.com>
2018-09-09 21:10:04 -0500

    Add files via upload
    Eliminated comments and got rid of prefix when messages are sent

--- server.py
+++ server.py
@@ -20,8 +20,6 @@ def accept_incoming_connections():
     
     while accepting:
         client, client_address = SERVER.accept()
-        # print("%s:%s has connected." % client_address)
-        # client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
     return
@@ -34,10 +32,6 @@ def handle_client(client):  # Takes client socket as argument.
     global BUFSIZ
 
     name = client.recv(BUFSIZ).decode("utf8")
-    # welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    # client.send(bytes(welcome, "utf8"))
-    # msg = "%s has joined the chat!" % name
-    # broadcast(bytes(msg, "utf8"))
     if name in clients.values():
         raise 'This name is already being used!'
     elif name == '':
@@ -48,22 +42,20 @@ def handle_client(client):  # Takes client socket as argument.
     while accepting:
         msg = client.recv(BUFSIZ)
         if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name + ": ")
+            broadcast(msg)
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(bytes("{quit}", "utf8") )
             client.close()
             del clients[client]
-            # broadcast(bytes("%s has left the chat." % name, "utf8"))
             break
     return
 
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
+def broadcast(msg):
     """Broadcasts a message to all the clients."""
     global clients
-    # print ('Broadcasting ', msg)
     for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+        sock.send(msg)
 
 
 def startserver(HOST, PORT):
