afc0636eda7b3363d2a068d01578bce5666dd6ad
Sabrina <sabrina.a.cook1997@gmail.com>
2018-10-04 13:40:49 -0500

    Captures webstream

---
+++ capture.py
@@ -0,0 +1,18 @@
+import cv2, time
+
+video-cv2.VideoCapture(0)
+a=0
+while True:
+    a=a+1
+    check, frame = video.read()
+    
+    print (check)
+    print(frame)
+    
+    cv2.imshow("capturing", frame)
+    
+    cv2.waitKey(1)
+    if key==ord('e'):
+        break
+print(a)
+video.release()
