3847a6382717c6a5f57bba5b2f27683deb412bdf
venny <arichasting@gmail.com>
2018-11-28 16:35:02 -0600

    Add try-except to speech recognizer

--- speech.py
+++ speech.py
@@ -4,7 +4,10 @@ r = sr.Recognizer()
 mic = sr.Microphone()
 
 def record_and_recognize():
-	with mic as source:
-		r.adjust_for_ambient_noise(source)
-		audio = r.listen(source)
-	return r.recognize_google(audio)
+	try:
+		with mic as source:
+			r.adjust_for_ambient_noise(source)
+			audio = r.listen(source)
+		return r.recognize_google(audio)
+	except:
+		return ""
